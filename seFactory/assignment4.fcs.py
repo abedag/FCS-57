51dfbc2c3cb26b03a5582403d6acf56b3b0dc425
abedag <abedalrahman7117@gmail.com>
2024-10-27 22:26:43 +0200

    new file:   seFactory/assignment4.fcs.py

---
+++ seFactory/assignment4.fcs.py
@@ -0,0 +1,9 @@
+print("Assignment 4:")
+print("")
+
+print("Exercise 1:")
+print("")
+
+class Matchingpairs:
+    def __init__(self,pairs):
+        self.match_pairs = ["(", ")", "{", "}", "[", "]"]
