a5544007d27a1e600b91cc4282ff55f118d64d7e
abedag <abedalrahman7117@gmail.com>
2024-10-28 03:24:46 +0200

    modified:   seFactory/assignment4.fcs.py

--- seFactory/assignment4.fcs.py
+++ seFactory/assignment4.fcs.py
@@ -1,9 +1,30 @@
-print("Assignment 4:")
-print("")
+print("Assignment 4:\n")
 
-print("Exercise 1:")
-print("")
+print("Exercise 1:\n")
 
-class Matchingpairs:
-    def __init__(self,pairs):
-        self.match_pairs = {"(" : ")", "{" : "}", "[" : "]"}
+class MatchingPairs:
+    def __init__(self):
+        self.match = {")": "(", "}": "{", "]": "["}    
+        self.element = []
+
+    def push(self, val):
+        for n in val:
+            if n in "({[":
+                self.element.append(n)
+
+            elif n in ")}]":
+                if not self.element or self.element.pop() != self.match[n]:
+                    return False
+        
+        return len(self.element) == 0
+
+c = MatchingPairs()
+
+print(c.push("(1+2)-3*[41+6]"))
+print(c.push("(1+2)-3*[41+6}"))
+print(c.push("(1+2)-3*[41+6"))
+print(c.push("(1+2)-3*]41+6["))
+print(c.push("(1+[2-3]*4{41+6})"))
+
+
+print("\nExercise 2:\n")
